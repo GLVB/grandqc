c272e0317a2ddf18ba1942517fe8691f175dab5b
GLVB <gborrecho@gmail.com>
2025-03-07 22:58:17 +0000

    updated

--- 01_WSI_inference_OPENSLIDE_QC/run_art.sh
+++ 01_WSI_inference_OPENSLIDE_QC/run_art.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # setting
-SLIDE_FOLDER="../Inputs/Paper_Original"
-OUTPUT_DIR="../Outputs/Paper_Original"
+SLIDE_FOLDER="../Inputs"
+OUTPUT_DIR="../Outputs"
 
 python wsi_tis_detect.py --slide_folder "$SLIDE_FOLDER" --output_dir "$OUTPUT_DIR"
 
